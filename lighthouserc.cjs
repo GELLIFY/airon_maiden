e55af3b62bd06f5b3ba421a5dbfaf5eca7d26c00
maltae <94902917+maltae@users.noreply.github.com>
2026-05-05 12:46:28 +0200

    Initialize feat/performance-budget

---
+++ lighthouserc.cjs
@@ -0,0 +1,21 @@
+const config = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'largest-contentful-paint': ['error', { maxNumericValue: 2500 }],
+        'cumulative-layout-shift': ['error', { maxNumericValue: 0.1 }],
+        'total-blocking-time': ['error', { maxNumericValue: 200 }],
+        'first-contentful-paint': ['error', { maxNumericValue: 1500 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
+
+module.exports = config
